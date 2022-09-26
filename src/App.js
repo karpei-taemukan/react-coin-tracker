2e13d98406b9bd7c605226462e6a522d9b623a84
karpeitaemukan <l07wkdgustn07l@gmail.com>
2022-09-27 00:48:45 +0900

    add exchange function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
 
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState("");
+  const [name, setName] = useState("");
 
   useEffect(()=>{
   fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
@@ -16,13 +18,46 @@ function App() {
   })
 },[]);
 
+const optionChange = (e) => {
+//console.log(e.target.value);
+coins.forEach((i) => {
+  if(i.name === e.target.value){
+    setValue(parseFloat(value / i.quotes.USD.price).toFixed(5));
+    const h1 = document.createElement("h1");
+      h1.innerText = i.name;
+      setName(i.name);
+  }
+})
+};
+
+const inputChange = (e) => {
+ setValue(e.target.value);
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  setValue("");
+};
+
   return (
     <div>
       <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : <select>
-      {coins.map((item) => (<option key={item.id}>{item.name}: {item.quotes.USD.price}</option>))}
-    </select>
-}
+      {loading ? <strong>Loading...</strong> : <select onChange={optionChange}>{coins.map((item) => (<option key={item.id}>{item.name}</option>))}</select>}
+    
+     <ul>
+    {coins.map((coin, index) => (
+      <li key={index}>{coin.name}:{coin.quotes.USD.price.toFixed(5)}</li>
+    ))}
+</ul>
+<form onSubmit={onSubmit}>
+<label htmlFor="coin">EXCHANGE USD TO COIN</label>
+<div>
+<input name="coin" value={value} type="text" placeholder="Exchange USD to coin" onChange={inputChange} />
+<button>RESET</button>
+</div>
+</form>
+{name ? <h1>{name}</h1> : null}
+{value ? <h1>{value}</h1> : null}
     </div>
   );
 }
